2e822c87d47382c5df990361586d8625bbaa7382
cpngMaika <phongutevippro@gmail.com>
2025-11-21 21:57:38 +0700

    first commit

---
+++ src/assets/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css"; // nhớ import CSS
+
+function Navbar() {
+  return (
+    <nav className="nav-container">
+      <div className="nav-brand">CPNGMAIKA</div>
+
+      <ul className="nav-menu">
+        <li><i class="fa-solid fa-house"></i><a href="#">Home</a></li>
+        <li><i class="fa-solid fa-box-archive"></i><a href="#">Archive</a></li>
+        <li><a href="#"><i class="fa-brands fa-github"></i>Github</a></li>
+      </ul>
+
+      <form className="nav-search">
+        <input type="text" placeholder="Search" />
+        <button type="submit"><i class="fa-solid fa-magnifying-glass"></i>Search</button>
+      </form>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/assets/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <>
+      {/* <aside class="sidebar mt-4 mx-5">Sidebar nè</aside> */}
+    </>
+  );
+}
+
+export default Sidebar;
